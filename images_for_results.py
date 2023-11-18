5b2615eb8bdc41d71404d9a4e0c0310e0744ae8a
mjanok <matus.janok@gmail.com>
2023-11-18 23:28:41 +0100

    4

--- images_for_results.py
+++ images_for_results.py
@@ -1,36 +1,26 @@
 import os
 import shutil
 
-
 def copy_files(src_folder, dst_folder, subdirs, start_time, end_time):
     for root, dirs, files in os.walk(src_folder):
-        print(f"Processing directory: {root}")
-
         # Check if all subdirs are present
-        if any(subdir in dirs for subdir in subdirs):
+        if root[9:11] in subdirs:
             last_dir = os.path.basename(root)
-            print(f"Last directory: {last_dir}")
-
-            # Check if the last directory is within the time range
-            if last_dir.isdigit() and int(last_dir) in range(start_time, end_time + 1):
-                src_path = os.path.join(root, last_dir)
-                dst_path = os.path.join(dst_folder, last_dir)
-                print(f"Copying files from {src_path} to {dst_path}")
-
-                # Create the destination directory if it doesn't exist
-                os.makedirs(dst_path, exist_ok=True)
+            time = root[15:19]
+            if time.startswith("0"):
+                time = int(time.lstrip("0"))
+            elif time:
+                time = int(time)
+            if time in range(start_time, end_time + 1):
+                # Get the parent directory (one level up from the last directory)
+                parent_dir = os.path.dirname(root)
+                src_path = os.path.join(parent_dir, last_dir)
 
                 # Iterate over files in the source directory and copy them to the destination
                 for filename in files:
                     src_file = os.path.join(src_path, filename)
-                    dst_file = os.path.join(dst_path, filename)
+                    dst_file = os.path.join(dst_folder, filename)
                     shutil.copy2(src_file, dst_file)
-
-                print(f"Files from {src_path} copied to {dst_path}")
-            else:
-                print(f"Last directory {last_dir} is not within the time range")
-        else:
-            print(f"Not all subdirectories {subdirs} are present in {dirs}")
 
 
 if __name__ == "__main__":
